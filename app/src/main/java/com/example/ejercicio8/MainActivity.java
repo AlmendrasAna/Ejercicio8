9f6768c0ab6dc564c794331f4255fe69c168314e
Ana <almendras451@gmail.com>
2023-06-23 00:39:52 -0400

    ejercicio 8 funciona pero el boton destruir hay presionar dos veces

---
+++ app/src/main/java/com/example/ejercicio8/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.ejercicio8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initLiteners();
+
+    }
+
+    private void initLiteners() {
+
+        Button secondActivityB = findViewById(R.id.buttonSecondActivity);
+        Button destroyB = findViewById(R.id.buttonDestroy);
+        secondActivityB.setOnClickListener(new View.OnClickListener() {
+             @Override
+    public void onClick(View v) {
+        Intent intent = new Intent( MainActivity.this, MainActivity2.class );
+    startActivity(intent);
+             }
+});
+        destroyB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+onDestroy();
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/com/example/ejercicio8/MainActivity2.java
@@ -0,0 +1,42 @@
+package com.example.ejercicio8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        initLiteners();
+    }
+
+    private void initLiteners() {
+
+        Button goToUrlB = findViewById(R.id.buttonGoUrl);
+        Button backB = findViewById(R.id.back);
+
+
+        goToUrlB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intertWeb = new Intent(Intent.ACTION_VIEW, Uri.parse("https://kotlindoc.blogspot.com/2019/10/android-log-con-timber.html"));
+                startActivity(intertWeb);
+            }
+        });
+
+        backB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+    }
+}
