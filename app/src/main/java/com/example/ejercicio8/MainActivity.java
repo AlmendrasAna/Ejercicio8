9099bddb517db022cfb368a35e8576243b367713
Ana <almendras451@gmail.com>
2023-06-23 15:00:35 -0400

    ejercicio 8 log agregados

--- app/src/main/java/com/example/ejercicio8/MainActivity.java
+++ app/src/main/java/com/example/ejercicio8/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.ejercicio8;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,8 +16,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         initLiteners();
+    Log.d(TAG,"onCreate ---Activity1");
 
     }
 
@@ -40,5 +45,7 @@ onDestroy();
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Log.d(TAG,"onDestroy ---");
+
     }
 }
--- app/src/main/java/com/example/ejercicio8/MainActivity2.java
+++ app/src/main/java/com/example/ejercicio8/MainActivity2.java
@@ -1,10 +1,13 @@
 package com.example.ejercicio8;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,7 +17,10 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Log.d(TAG,"onCreate Activity2 ---");
         initLiteners();
+
     }
 
     private void initLiteners() {
